0552577b681e16a2dac21c769cb100c8b522c974
Matheus Rodrigues <rmatheus440@gmail.com>
2025-04-07 20:24:56 -0300

    Update para carregar o saldo inicial

--- src/app/saldoInicial/page.tsx
+++ src/app/saldoInicial/page.tsx
@@ -1,28 +1,12 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation';
-import CriarSaldoInicial from './components/CriarSaldoInicial';
+import { Suspense } from 'react';
+import PageContent from './components/PageContent';
 
 export default function Page() {
-  const searchParams = useSearchParams();
-
-  const idBanco = Number(searchParams.get('idBanco'));
-  const idCliente = Number(searchParams.get('idCliente'));
-
-  if (!idBanco || !idCliente) {
-    return (
-      <div className="p-4 text-red-600">
-        ⚠️ Erro: parâmetros obrigatórios ausentes na URL (idBanco, idCliente).
-      </div>
-    );
-  }
-
   return (
-    <div className="p-6">
-      <CriarSaldoInicial
-        idBanco={idBanco}
-        idCliente={idCliente}
-      />
-    </div>
+    <Suspense fallback={<div className="p-6">Carregando formulário...</div>}>
+      <PageContent />
+    </Suspense>
   );
 }
